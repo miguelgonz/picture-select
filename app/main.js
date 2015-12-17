8159482619fd1cc5f5ec96a177885ce077e7c05d
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-12-17 22:36:48 +0000

    main javascript file

---
+++ app/main.js
@@ -0,0 +1,16 @@
+var React = require('./lib/react.js');
+var Pictures = require('./pictures.js');
+
+(function(){
+    window.cb = function (data){
+        React.render(
+            React.createElement(Pictures),
+            document.getElementById('app')
+        );
+    }
+
+    var tags='london';
+    var script=document.createElement('script');
+    script.src='http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=cb&tags='+tags;
+    document.head.appendChild(script);
+})();
